83c33fa9f1a65f455cde56e51563583df8ebec88
saba manchkhashvili <saba.manchkhashvili23@gmail.com>
2025-06-01 17:04:23 +0400

    added svelte and simple map (svelte-maplibre-gl)

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { text } from '@sveltejs/kit';
+import { MapLibre } from 'svelte-maplibre-gl';
+</script>
+
+<MapLibre 
+  class="h-[100vh]"
+  style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+  >
+  
+</MapLibre>
+
+<style>
+  @reference "tailwindcss";
+</style>
