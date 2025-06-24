f9e74d42d9d7617b867d0131e83d04082ef98ace
skamiXD <saba.manchkhashvili23@gmail.com>
2025-06-24 22:55:16 +0400

    changed popup to follow cursor

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -7,9 +7,15 @@
 
 <script lang="ts">
   import L from 'leaflet'
-	import { spring } from "svelte/motion"
   import { fly } from "svelte/transition";
+	import { spring, tweened } from "svelte/motion"
 
+	const mouseCoords = spring({ x: 0, y: 0 })
+	const scale = tweened(1, { duration: 100 })
+
+	const onMouseMove = (event) => {
+		$mouseCoords = { x: event.x, y: event.y }
+	}
 
   var info:object = {}
   var AiInput:string = ""
@@ -18,6 +24,9 @@
 	var response = "";
 	var loading = false
   var questions = []
+	var popupImgUrl = ""
+	var popupName = ""
+	var popupOpacity = 0;
 
   var cooResult:Array<L.LatLngExpression[]> = [];
 
@@ -65,18 +74,27 @@
       cooResult.map((el, index) => {
 				var polygon = L.polygon(el).addTo(map);
 				polygon.addEventListener("click", async() => {await getInfo(arr[index].ID); await getAiQuesions(arr[index].ID); response = ""; aiResponding = false});
-				polygon.bindPopup('<div class="w-30 font-sans antialiased">' + '<img class="w-full rounded-lg object-cover shadow-sm mb-2" src="' + arr[index].ImageURL + '"/>' + '<p class="text-center text-sm font-semibold text-slate-700 leading-tight break-words px-2 py-1">' + arr[index].Name + '</p>' + '</div>');
 				polygon.on("mouseover", (e) => {
-					polygon.openPopup();
+					popupImgUrl = arr[index].ImageURL;
+					popupName = arr[index].Name;
+					popupOpacity = 100;
 				})
 				polygon.on("mouseout", (e) => {
-					polygon.closePopup();
+					popupOpacity = 0;
 				})
       })
     })
   }
 </script>
 
+<svelte:window 
+	on:mousemove={onMouseMove} 
+/>
+
+<div style:--x={`${$mouseCoords.x}px`} style:--y={`${$mouseCoords.y - 100}px`} style:opacity={popupOpacity / 100} class="absolute top-0 left-0 w-[10vh] z-[999] transform translate-x-[calc(-50%_+_var(--x,_0px))] translate-y-[calc(-50%_+_var(--y,_0px))] scale-[var(--scale,1)] pointer-events-none bg-white p-2 rounded-lg shadow-xl ring-1 ring-black ring-opacity-5 flex flex-col items-center transition-opacity duration-300 ease-in-out">
+	<img class="w-full rounded-lg object-cover shadow-sm mb-2" src={popupImgUrl}/>
+	<p class="text-center text-xs font-semibold text-slate-700 leading-tight break-words px-1 py-0.5"> {popupName} </p>
+</div>
 
 <div class="flex relative">
   {#if Object.keys(info).length > 0}
